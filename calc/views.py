7f2a03bdb64728ef8d5cd4cb002fdc527eb8674b
Stivi7 <stivi.ndoni@gmail.com>
2018-01-03 16:56:03 +0100

    building calc

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+context = {
+    'result': 0,
+}
+
+def home(request):
+    return render(request, 'calc/home.html', context)
+
+def calculate(request):
+    if (request.method == 'POST'):
+        ex = request.POST['numField']
+        print(ex)
+        result = eval(ex)
+        context['result'] = result
+    return redirect('home')
--- calculator/urls.py
+++ calculator/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from calc import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name="home"),
+    url(r'^calculate/$', views.calculate, name="calculate" ),
 ]
